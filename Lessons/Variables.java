7b317d6eb0f0e7a3b92f3125382bc69a4ec40f00
Jacobdye99 <Jacobdye99@gmail.com>
2022-04-04 23:12:21 -0400

    Learning more about strings

--- Lessons/Variables.java
+++ Lessons/Variables.java
@@ -1,10 +1,22 @@
 public class Creator {
 	public static void main(String[] args) {
+        // Strings 
+        //  \n pushes the text after it to the next line
+        // "\"Hello World\"" Will print "Hello World" instead of Hello World
+        //  \\ will allow us to place \ in the string and print it
     String name = "James Gosling";
+        // Integer
     int yearCreated = 1995;
+        // Decimals
+    double androidShare = 81.7;
+        // Boolean
+    boolean intsCanHoldDecimals = false;
+        // Single character
+    char expectedGrade = 'A';
+    System.out.println(expectedGrade);
+    System.out.println(intsCanHoldDecimals);
     System.out.println(name);
     System.out.println(yearCreated);
-    double androidShare = 81.7;
     System.out.println(androidShare);
     
 	}
