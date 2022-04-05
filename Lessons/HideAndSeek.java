f83c7f5c173d313691d3a48237d914ae5e8044c8
Jacobdye99 <Jacobdye99@gmail.com>
2022-04-04 22:12:20 -0400

    Adding Comments

--- Lessons/HideAndSeek.java
+++ Lessons/HideAndSeek.java
@@ -1,6 +1,8 @@
 public class HideAndSeek {
   public static void main(String[] args) {
+    // The println keeps the cursor at the end of this line
     System.out.println("Let's play hide and seek.");
+    // Print keeps the cursor at the end of the last println line
     System.out.print("Three...");
     System.out.print("Two...");
     System.out.println("One...");
